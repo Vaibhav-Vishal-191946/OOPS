7467179ff7ada1fccb7dee8f3965bc1c0746cf62
Vaibhav-Vishal-191946 <vvaibhav728@gmail.com>
2021-03-01 15:14:09 +0530

    Second commit

--- OOPSHW 1.cpp
+++ OOPSHW 1.cpp
@@ -1,37 +1,37 @@
-//WAP to read a matrix of size m x n form the keyboard and display the same using functions.
+//WAP to read a matrix of size m x n from the keyboard and display the same using functions.
 #include <iostream>
 using namespace std;
-int matrix[100][100];
 void read_matrix(int,int);
 void display_matrix(int,int);
+int array_2d[100][100];
 int main()
 {
-    int m, n;
-    cout << "Enter the Number Of Rows - ";
+    int m,n;
+    cout << "Required Rows - ";
     cin >> m;
-    cout << "Enter the Number Of Columns - ";
+    cout << "Required Columns - ";
     cin >> n;
+    cout<<endl;
     read_matrix(m,n);
+    cout<<endl;
     display_matrix(m,n);
     return 0;
 }
 void read_matrix(int row, int column)
 {
 	cout << "Enter the elements of "<<row<<" x "<<column<<" Matrix:" <<endl;
-    for (int i = 0; i < row; i++) {
+    for (int i = 0; i < row; i++) 
         for (int j = 0; j < column; j++) {
             cout << "Element at ["<<i<<"]["<<j<<"] = ";       
-            cin >> matrix[i][j];
-        }
-        cout << endl;
-    }    
+            cin >> array_2d[i][j];
+        }   
 }
 void display_matrix(int row, int column)
 {
-	cout << "Entered matrix is :" << endl;
+	cout << "Entered "<<row<<" x "<<column<<" Matrix is:" << endl;
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < column; j++)
-            cout <<matrix[i][j]<<"\t";       
+            cout <<array_2d[i][j]<<"\t";       
         cout<<endl;
     }  
 }
