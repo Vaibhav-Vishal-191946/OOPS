df79add1ff6448ebab83198d556a38a149633d2b
Vaibhav-Vishal-191946 <vvaibhav728@gmail.com>
2021-03-10 12:03:29 +0530

    update

---
+++ OOPSHW3.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+float power(float,int);
+float power(float);
+void display(float);
+int main() {
+	float m, result;
+	int n;
+	cout << "Enter Base :";
+	cin >> m;
+	cout << "Enter Power(for default,press -999):";
+	cin >> n;
+	if(n== -999)
+		result = power(m);
+	else
+		result = power(m, n);
+	display(result);
+	
+	return 0;
+}
+float power(float base, int exponent) {
+		float results = 1;
+		for (int i = 0; exponent > i; ++i)
+			results = results * base;
+		return results;
+}
+float power(float base) {
+	int exponent = 2;
+	float results = 1;
+	for (int i = 0; exponent > i; ++i)
+		results = results * base;
+	return results;
+}
+void display(float value) {
+		cout << "The Result is:" << value;
+}
